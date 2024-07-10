b55d35d90a763b0aa59df8e64526c5d15be0c94d
Abigirl Muchineripi <muchineripiabby@gmail.com>
2024-07-10 11:51:29 +0200

    Renedering with Best Practices

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,17 +2,16 @@ function ListGroup() {
   let items = ["Harare", "Bulawayo", "Victoria Falls", "Gweru", "Mutare"];
   items = [];
 
-  if (items.length === 0)
-    return (
-      <>
-        <h1>List</h1>
-          <p>No item found</p>        
-      </>
-    );
+  
+  const getMessage = () => {
+    return items.length === 0 ? <p>No item found</p>  : null;
+
+  }
 
   return (
     <>
       <h1>List</h1>
+      { getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
