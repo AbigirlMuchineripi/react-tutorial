9a2971914cccec535ae561df22f2ff90d3d96e80
Abigirl Muchineripi <muchineripiabby@gmail.com>
2024-07-10 11:56:46 +0200

    Updated rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,11 @@
 function ListGroup() {
   let items = ["Harare", "Bulawayo", "Victoria Falls", "Gweru", "Mutare"];
-  items = [];
-
-  
-  const getMessage = () => {
-    return items.length === 0 ? <p>No item found</p>  : null;
-
-  }
+  items = []; 
 
   return (
     <>
       <h1>List</h1>
-      { getMessage()}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
